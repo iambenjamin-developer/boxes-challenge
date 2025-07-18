6288549965ff8e70df57f8764ff7e535d663c3c8
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-18 16:28:20 -0300

    get workshops

---
+++ src/API/Controllers/LeadsController.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTOs.Leads;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadsController : ControllerBase
+    {
+        private readonly IWorkshopService _workshopService;
+
+        public LeadsController(IWorkshopService workshopService)
+        {
+            _workshopService = workshopService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LeadRequestDto request)
+        {
+            var result = await _workshopService.GetActiveWorkshopsAsync();
+            return Ok(result);
+        }
+
+
+    }
+}
