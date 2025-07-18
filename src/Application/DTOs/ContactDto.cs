32882406169e0ec0432f3e91804ec54bfd6d206a
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-18 16:06:15 -0300

    Add request DTO

---
+++ src/Application/DTOs/ContactDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application.DTOs
+{
+    public class ContactDto
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+    }
+}
---
+++ src/Application/DTOs/VehicleDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application.DTOs
+{
+    public class VehicleDto
+    {
+        [JsonPropertyName("make")]
+        public string Make { get; set; }
+
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+
+        [JsonPropertyName("license_plate")]
+        public string LicensePlate { get; set; }
+    }
+}
