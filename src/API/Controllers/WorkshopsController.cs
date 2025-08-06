d2cf9b226e113e17aceb2f67602708402abcde91
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-08-06 14:12:35 -0300

    Workshop endpoint

---
+++ src/API/Controllers/WorkshopsController.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs.Workshops;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkshopsController : ControllerBase
+    {
+        private readonly IWorkshopService _workshopService;
+
+        public WorkshopsController(IWorkshopService workshopService)
+        {
+            _workshopService = workshopService;
+        }
+
+
+        /// <summary>
+        /// Obtiene la lista de todos los workshops.
+        /// </summary>
+        /// <remarks>
+        /// Este endpoint devuelve todos los workshops que se consultan por la API externa.
+        /// </remarks>
+        /// <response code="200">Lista de workshops obtenida correctamente.</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<WorkshopDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<WorkshopDto>>> GetAll()
+        {
+            var result = await _workshopService.GetActiveWorkshopsAsync();
+            return Ok(result);
+        }
+
+    }
+}
